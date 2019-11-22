8d790935f9ad9cfbc40c01006fee82c0dceeba59
hira22 <masayuki.hiraoka@ietty.co.jp>
2019-11-22 10:23:55 +0900

    Initial Commit

---
+++ unwind_or_dismissTests/unwind_or_dismissTests.swift
@@ -0,0 +1,34 @@
+//
+//  unwind_or_dismissTests.swift
+//  unwind_or_dismissTests
+//
+//  Created by hiraoka on 2019/11/22.
+//  Copyright © 2019 hiraoka. All rights reserved.
+//
+
+import XCTest
+@testable import unwind_or_dismiss
+
+class unwind_or_dismissTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
